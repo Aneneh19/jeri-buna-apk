32b1b8b81c405bb5a4e7730aa28cf663003aa3a2
Aneneh19 <104998690+Aneneh19@users.noreply.github.com>
2026-03-19 17:11:12 +0300

    Create MainActivity.java

---
+++ app/src/main/java/com/jeribuna/app/MainActivity.java
@@ -0,0 +1,47 @@
+package com.jeribuna.app;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getActionBar() != null) getActionBar().hide();
+        webView = new WebView(this);
+        setContentView(webView);
+        WebSettings s = webView.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        s.setDatabaseEnabled(true);
+        s.setAllowFileAccess(true);
+        s.setAllowContentAccess(true);
+        s.setCacheMode(WebSettings.LOAD_DEFAULT);
+        s.setLoadWithOverviewMode(true);
+        s.setUseWideViewPort(true);
+        s.setBuiltInZoomControls(false);
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) webView.goBack();
+        else moveTaskToBack(true);
+    }
+
+    @Override
+    protected void onResume() { super.onResume(); webView.onResume(); }
+
+    @Override
+    protected void onPause() { super.onPause(); webView.onPause(); }
+}
